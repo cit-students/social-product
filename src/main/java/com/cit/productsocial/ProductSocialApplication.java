5902b3f70c64bd3b32b73840ada8b36d9e1a1098
thaihuy711 <thaihuy7191112@gmail.com>
2017-12-18 10:15:17 +0700

    Init Product social

---
+++ src/main/java/com/cit/productsocial/ProductSocialApplication.java
@@ -0,0 +1,12 @@
+package com.cit.productsocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductSocialApplication.class, args);
+	}
+}
